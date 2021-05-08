29961919056cbf365099ee00371df24d11f71536
chris <ckim0707@gmail.com>
2021-05-08 12:59:21 -0500

    Week 1 React Assignment Revised

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -19,9 +19,23 @@ class Bootcamp {
             this.students.push(student);
             console.log(`Registering ${student.email} to the bootcamp ${this.name}.`);
         }
+        return this.students;
     }
 }
         
 
 const chris = new Student("Chris", "chris@nucamp.com", "Austin");
 const fullStack = new Bootcamp("Full Stack Web Development", "Advanced");
+
+fullStack.registerStudent(chris);
+fullStack.registerStudent(chris);
+
+// The way the instructor did it
+// if (this.students.filter(s => s.email === student.email).length === 1))
+
+//2 Step
+//const filter = this.students.filter(s => s.email === student.email)
+//const isRegistered = filter.length
+//if(isRegistered === 1) {
+//     console.log('Student is already registered');
+// }
